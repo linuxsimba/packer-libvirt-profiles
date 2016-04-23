f1bb73d6101baa30c1752f7510ca4d74af8c9f06
stanley karunditu <stanley@linuxsimba.com>
2016-04-23 12:03:44 -0400

    add update.sh

---
+++ scripts/centos/update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -eux
+
+# should output one of 'redhat' 'centos' 'oraclelinux'
+distro="`rpm -qf --queryformat '%{NAME}' /etc/redhat-release | cut -f 1 -d '-'`"
+
+# update centos packages to the latest
+yum -y upgrade
+
